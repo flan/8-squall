e24bb2bd9e2d540ebd138ec2cdff6ec62c3fd817
Neil Tallim <flan@uguu.ca>
2021-04-02 14:39:18 -0600

    Added alias for !convert

--- lib8squall_plugins/convert/__init__.py
+++ lib8squall_plugins/convert/__init__.py
@@ -13,7 +13,7 @@ from . import volume
 from . import weight
 
 def get_help_summary(client, message):
-    return ("`!conv [quantity] <unit> [to] <unit>` for scale conversion.",)
+    return ("`!conv[ert] [quantity] <unit> [to] <unit>` for scale conversion.",)
 
 _QUERY_RE = re.compile(r'(?P<qty>-?\d*\.?\d*)?\s*(?P<unit1>["\'\w]+)\s+(?:to\s+)?(?P<unit2>["\'\w]+)')
 
@@ -30,39 +30,40 @@ def _process(module, quantity, unit1, unit2):
     return (result, normalised_unit1, normalised_unit2)
     
 async def handle_message(client, message):
-    if message.content.startswith('!conv '):
-        query_match = _QUERY_RE.search(message.content[6:])
-        if query_match:
-            unit1 = query_match.group('unit1').title()
-            unit2 = query_match.group('unit2').title()
-            
-            quantity = query_match.group('qty')
-            if quantity is None or quantity == '.': #the regex allows '.' for the sake of readability
-                quantity = 1.0
-            else:
-                quantity = float(quantity)
+    for trigger in ('!conv ', '!convert '):
+        if message.content.startswith(trigger):
+            query_match = _QUERY_RE.search(message.content[len(trigger):])
+            if query_match:
+                unit1 = query_match.group('unit1').title()
+                unit2 = query_match.group('unit2').title()
                 
-            for module in (
-                temperature,
-                length,
-                weight,
-                volume,
-                angle,
-                timeconv,
-                area,
-                energy,
-                force,
-                pressure,
-            ):
-                result = _process(module, quantity, unit1, unit2)
-                if result:
-                    (result, normalised_unit1, normalised_unit2) = result
-                    await message.reply("{:,.2f}{} = {:,.2f}{}".format(
-                        quantity, normalised_unit1,
-                        result, normalised_unit2,
-                    ))
-                    break
-            else:
-                await message.reply("No conversion available for {} and {}.".format(unit1, unit2))
-        return True
+                quantity = query_match.group('qty')
+                if quantity is None or quantity == '.': #the regex allows '.' for the sake of readability
+                    quantity = 1.0
+                else:
+                    quantity = float(quantity)
+                    
+                for module in (
+                    temperature,
+                    length,
+                    weight,
+                    volume,
+                    angle,
+                    timeconv,
+                    area,
+                    energy,
+                    force,
+                    pressure,
+                ):
+                    result = _process(module, quantity, unit1, unit2)
+                    if result:
+                        (result, normalised_unit1, normalised_unit2) = result
+                        await message.reply("{:,.2f}{} = {:,.2f}{}".format(
+                            quantity, normalised_unit1,
+                            result, normalised_unit2,
+                        ))
+                        break
+                else:
+                    await message.reply("No conversion available for {} and {}.".format(unit1, unit2))
+            return True
     return False
