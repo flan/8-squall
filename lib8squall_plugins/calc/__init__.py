b9c3b8d23914c5aaf17559c8f402b2a8dbd5b392
Neil Tallim <flan@uguu.ca>
2022-01-10 13:58:47 -0700

    Calculator always supplied data with help-text

--- lib8squall_plugins/calc/__init__.py
+++ lib8squall_plugins/calc/__init__.py
@@ -2,12 +2,15 @@
 from . import calc
 
 def get_help_summary(client, message):
+    session = calc.Session()
     return (
         "Calculator",
         (
             "`!calc <math> [;<math>]` will perform calculations; the equation-format is the common scholastic standard.",
-            "`<math>` can be variable-assignment (`x = 22 * sqrt(9)`), functions (`f(x) = x * 2`), or equations (`x + 2 / f(7)`); the order in which these are supplied does not matter, but at least one equation must be performed to produce output.",
-            "`!calc list` will list all built-in structures.",
+            "`<math>` can be variable-assignment (`x = 22 * sqrt(9)`), functions (`f(x) = x * 2`), or expressions (`x + 2 / f(7)`); the order in which these are supplied does not matter, but at least one expression must be performed to produce output.",
+            "Built-in operators: {}".format(', '.join('`{}`'.format(i) for i in ('+', '-', '*', '/', '\\', '^', '%', '<', '>',))),
+            "Built-in functions: {}".format(', '.join('`{}`'.format(i) for i in (session.listFunctions()))),
+            "Built-in constants: {} (`?` is used for linear-equation-solving)".format(', '.join('`{}`'.format(i) for i in (session.listVariables()))),
             "Linebreaks can be used in place of semicolons for readability.",
             "Function parameters must be scalars or variables; directly nesting a function will prevent expression-compilation.",
         ),
@@ -27,35 +30,22 @@ async def handle_message(client, message):
         request = message.content[6:]
         request_lower = request.lower()
         
-        if request_lower == 'list':
-            session = calc.Session()
+        try:
+            session = calc.Session(request.replace('\n', ';'))
+            (variables, equations) = session.evaluate()
             
-            output = []
-            output.append("Built-in operators:")
-            output.append("`{}`".format(',  '.join(('+', '-', '*', '/', '\\', '^', '%', '<', '>'))))
-            output.append("Built-in functions:")
-            output.append("`{}`".format(',  '.join(session.listFunctions())))
-            output.append("Built-in variables:")
-            output.append("`{}`".format(',  '.join(session.listVariables())))
-            
-            await message.reply('\n'.join(output))
-        else:
-            try:
-                session = calc.Session(request.replace('\n', ';'))
-                (variables, equations) = session.evaluate()
-                
-                if equations:
-                    output = []
-                    output.extend("- `{} = {}`".format(name, _try_int(value)) for (name, value) in variables)
-                    output.extend("`{}` = `{}`".format(equation, _try_int(value)) for (equation, value) in equations)
-                    await message.reply('\n'.join(output))
-                else:
-                    await message.reply("No expressions provided.")
-            except Exception as e:
-                await message.reply("Something went wrong. {}: {}".format(
-                    e.__class__.__name__,
-                    e,
-                ))
-                raise
+            if equations:
+                output = []
+                output.extend("- `{} = {}`".format(name, _try_int(value)) for (name, value) in variables)
+                output.extend("`{}` = `{}`".format(equation, _try_int(value)) for (equation, value) in equations)
+                await message.reply('\n'.join(output))
+            else:
+                await message.reply("No expressions provided.")
+        except Exception as e:
+            await message.reply("Something went wrong. {}: {}".format(
+                e.__class__.__name__,
+                e,
+            ))
+            raise
         return True
     return False
